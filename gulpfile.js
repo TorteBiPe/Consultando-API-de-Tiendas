e2b0801b020d5f8b280dd2b11fecfabceca6aeaf
TorteBiPe <bienesperezraul@gmail.com>
2021-02-16 23:05:57 +0000

    check

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sass = require('gulp-sass');
 const jsdoc = require('gulp-jsdoc3');
 const {src, task} = require('gulp');
 const eslint = require('gulp-eslint');
-
+ 
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
@@ -32,21 +32,22 @@ gulp.task('serve', gulp.series(['sass'], function() {
 }));
 
 gulp.task('doc', function (cb) {
-  gulp.src(['README.md', './src/**/*.js'], {read: false})
+  gulp.src(['./src/**/*.js'], {read: false})
       .pipe(jsdoc(cb));
 });
 
 task('default', () => {
-  return src(['./js/*.js'])
-      // eslint() attaches the lint output to the "eslint" property
-      // of the file object so it can be used by other modules.
-      .pipe(eslint())
-      // eslint.format() outputs the lint results to the console.
-      // Alternatively use eslint.formatEach() (see Docs).
-      .pipe(eslint.format())
-      // To have the process exit with an error code (1) on
-      // lint error, return the stream and pipe to failAfterError last.
-      .pipe(eslint.failAfterError());
-}); 
+    return src(['./js/*.js'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
+});
+
 
 gulp.task('default', gulp.series(['serve']));
