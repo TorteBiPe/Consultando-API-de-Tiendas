581cfc3e61ef3c8e7bdf2bbbfce617a02f2baa48
TorteBiPe <bienesperezraul@gmail.com>
2021-02-16 21:26:59 +0000

    confirmar

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,32 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
-  
-  exports.default = defaultTask
+'use strict';
+ 
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./css/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./css/*.scss', gulp.series(['sass']));
+});
+
+gulp.task('serve', gulp.series(['sass'], function() {
+
+  browserSync.init({
+      server: "./"
+  });
+
+  gulp.watch("./css/*.scss", gulp.series(['sass']));
+  gulp.watch("./index.html").on('change', browserSync.reload);
+  gulp.watch("./js/*.js").on('change', browserSync.reload);
+}));
+
+
+gulp.task('default', gulp.series(['serve']));
